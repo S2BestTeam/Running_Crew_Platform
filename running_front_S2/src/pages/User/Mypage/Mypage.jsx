4f36227dfe24e388f34d6205fab44c7c8e071fb0
kksun0202 <kksun0202@naver.com>
2025-08-10 20:40:22 +0900

    mypage 수정

--- running_front_S2/src/pages/User/Mypage/Mypage.jsx
+++ running_front_S2/src/pages/User/Mypage/Mypage.jsx
@@ -7,6 +7,17 @@ import api from "../../../api/axios";
 import usePrincipalQuery from "../../../queries/usePrincipalQuery";
 
 function Mypage() {
+  const [gunguList, setGunguList] = useState([]);
+  const [nickname, setNickname] = useState("");
+  const [isNicknameChecked, setIsNicknameChecked] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [gender, setGender] = useState("");
+
+  const [errors, setErrors] = useState({
+    nickname: "",
+    phoneNumber: "",
+  });
+
   const principalQuery = usePrincipalQuery();
   const userInfo = principalQuery.data?.data?.body.user;
 
@@ -26,6 +37,37 @@ function Mypage() {
     };
 
     fileInput.click();
+  };
+
+  const handleNicknameChange = (e) => {
+    const value = e.target.value;
+    setNickname(value);
+    setIsNicknameChecked(false);
+    const errorMsg = validateField("nickname", value);
+    setErrors((prev) => ({ ...prev, nickname: errorMsg }));
+  };
+
+  const handleCheckNickname = async () => {
+    if (!nickname.trim()) return;
+    try {
+      const response = await reqCheckNickname(nickname);
+      const isAvailable = response.data.body === "false";
+      if (isAvailable) {
+        setIsNicknameChecked(true);
+        alert("사용 가능한 닉네임입니다!");
+      } else {
+        alert("중복된 닉네임입니다.");
+      }
+    } catch {
+      alert("중복확인 중 오류가 발생했습니다.");
+    }
+  };
+
+  const handlePhoneChange = (e) => {
+    const value = e.target.value;
+    setPhoneNumber(value);
+    const errorMsg = validateField("phoneNumber", value);
+    setErrors((prev) => ({ ...prev, phoneNumber: errorMsg }));
   };
 
   return (
@@ -49,19 +91,49 @@ function Mypage() {
               </div>
             </div>
 
-            <div>이름: {userInfo?.fullName}</div>
-            <div>이메일: {userInfo?.email}</div>
-            <div>닉네임: {userInfo?.nickname}</div>
-            <div>생년월일: {userInfo?.birthDate}</div>
-            <div>연락처: {userInfo?.phoneNumber}</div>
-            <div>
-              성별:{" "}
-              {userInfo?.gender == 1
-                ? "남자"
-                : userInfo?.gender == 2
-                ? "여자"
-                : ""}
-            </div>
+            <div>이름</div>
+            <input disabled value={userInfo?.fullName}/>
+            <div>이메일</div>
+            <input disabled value={userInfo?.email}/>
+            <div>닉네임:</div>
+            <input
+              type="text"
+              value={userInfo?.nickname}
+              onChange={handleNicknameChange}
+              required
+            />
+            <button onClick={handleCheckNickname} disabled={!nickname.trim()}>
+              {isNicknameChecked ? "❤️ 사용 가능!" : "닉네임 중복 확인"}
+            </button>
+            {errors.nickname && <p style={{ color: "red" }}>{errors.nickname}</p>}
+
+            <div>생년월일</div>
+            <input disabled value={userInfo?.birthDate}/>
+            <div>연락처</div>
+            <input
+              type="tel"
+              value={userInfo?.phoneNumber}
+              onChange={handlePhoneChange}
+              required
+            />
+            {errors.phoneNumber && (
+              <p style={{ color: "red" }}>{errors.phoneNumber}</p>
+            )}
+            <div css={gender}>
+              <div>성별</div>
+              <div
+                css={[box, gender === "1" && selectedBox]}
+                onClick={() => setGender("1")}
+              >
+                남자
+              </div>
+              <div
+                css={[box, gender === "2" && selectedBox]}
+                onClick={() => setGender("2")}
+              >
+                여자
+              </div>
+          </div>
             <div>도시: {userInfo?.gungu.gunguName}</div>
           </div>
         </div>
--- running_front_S2/src/pages/User/Mypage/styles.js
+++ running_front_S2/src/pages/User/Mypage/styles.js
@@ -5,3 +5,25 @@ export const container = css`
   width: 100%;
   height: 100%;
 `;
+
+
+const gender = css`
+  display: flex;
+  gap: 10px;
+  max-width: 300px;
+`;
+
+const box = css`
+  flex: 1;
+  border-radius: 6px;
+  padding: 12px 24px;
+  cursor: pointer;
+  text-align: center;
+  border: 2px solid #ccc;
+  background-color: #fff;
+`;
+
+const selectedBox = css`
+  border-color: #4a90e2;
+  background-color: #eaf2fc;
+`;
